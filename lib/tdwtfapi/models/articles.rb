8aba99d505f3537ce3f0e3a6ef8d68ea80a52763
Ryan Priebe <rpriebe@me.com>
2016-01-07 10:04:53 -0700

    added article query methods

--- lib/tdwtfapi/models/articles.rb
+++ lib/tdwtfapi/models/articles.rb
@@ -3,8 +3,44 @@ module TDWTF
     
     ENDPOINT_PREFIX = :articles
 
-    def article(id)
-      request("#{ENDPOINT_PREFIX}/id/#{id}")
+    #
+    # since 0.1.0
+    def by_id(query)
+      request("#{ENDPOINT_PREFIX}/id/#{query}")
+    end
+
+    #
+    # since 0.1.0
+    def by_slug(query)
+      request("#{ENDPOINT_PREFIX}/slug/#{query}")
+    end
+
+    #
+    # since 0.1.0
+    def by_date(month, year)
+      if year < 1999
+        raise "Invalid year argument provided, must be greater than 1999"
+      end
+
+      request("#{ENDPOINT_PREFIX}/#{year}/#{month}")
+    end
+
+    #
+    # since 0.1.0
+    def random
+      request("#{ENDPOINT_PREFIX}/random")
+    end
+
+    #
+    # since 0.1.0
+    def recent(num = nil)
+      endpoint = "#{ENDPOINT_PREFIX}/recent"
+
+      if num
+        endpoint = "#{ENDPOINT_PREFIX}/recent/#{num}"
+      end
+
+      request(endpoint)
     end
 
   end
